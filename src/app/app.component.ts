9fd2a6819f398b39aad35fa842cb0c0e19312e32
Dinh Nguyen Tien <bibo.dnt.159@gmail.com>
2021-02-18 12:50:55 +0100

    CV1 - read file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,4 @@ import {Component, Input} from '@angular/core';
 })
 export class AppComponent {
   title = 'ads-greedy-cviko1';
-
-  something = 0;
-
-  // tslint:disable-next-line:use-lifecycle-interface
-  ngOnInit(): void {
-    console.log('I am here');
-    this.something = 25;
-  }
-
-  changeSomething(): void {
-    this.something = 69;
-    console.log('CLICK');
-  }
-
-
 }
