a22fa6c8746ae2b210188b6b50ea90106637fb24
Dinh Nguyen Tien <bibo.dnt.159@gmail.com>
2021-02-18 11:26:27 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ads-greedy-cviko1';
+
+  something = 0;
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnInit(): void {
+    console.log('I am here');
+    this.something = 25;
+  }
+
+  changeSomething(): void {
+    this.something = 69;
+    console.log('CLICK');
+  }
+
+
 }
