544cb76a8755f1acd35bdcf3e0b66f3074e41fba
Dinh Nguyen Tien <bibo.dnt.159@gmail.com>
2021-02-25 20:44:43 +0100

    CV2

--- src/app/cviko2/cviko2.component.ts
+++ src/app/cviko2/cviko2.component.ts
@@ -38,10 +38,10 @@ export class Cviko2Component implements OnInit {
   dynamicProg(towns: number[]): number {
     const ideal = 50;
     const pen = [];
-    const skuska = [];
 
     // pre kazde mesto hlada idealne "cestu" s najmensou penalizaciou
-    // a ak chcem zistit idealne penalizaciu dalsieho mesta, tak prejdem vsetky mesta uz s najdenymi idealnymi penalmi a zistim, s akym mestom je najmensia penalizacia
+    // a ak chcem zistit idealne penalizaciu dalsieho mesta, tak prejdem vsetky mesta uz s najdenymi idealnymi penalmi a zistim,
+    //      s akym mestom je najmensia penalizacia
     towns.forEach( (town, index) => { // v 604
       pen.push(Math.pow(ideal - town, 2)); // 400 - 602 = power -> 40 804
       for (let pIndex = 0; pIndex < index; pIndex++) {
@@ -49,7 +49,6 @@ export class Cviko2Component implements OnInit {
         const poss = pen[pIndex] + Math.pow(ideal - dif, 2);
         if (poss < pen[index]) {
           pen[index] = poss;
-          skuska.push(pIndex);
         }
       }
     });
