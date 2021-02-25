3349aa1e445515f362a6bba45914fa6e52cbb4b6
Dinh Nguyen Tien <bibo.dnt.159@gmail.com>
2021-02-25 20:43:34 +0100

    CV2 - comment

--- src/app/cviko2/cviko2.component.ts
+++ src/app/cviko2/cviko2.component.ts
@@ -40,6 +40,8 @@ export class Cviko2Component implements OnInit {
     const pen = [];
     const skuska = [];
 
+    // pre kazde mesto hlada idealne "cestu" s najmensou penalizaciou
+    // a ak chcem zistit idealne penalizaciu dalsieho mesta, tak prejdem vsetky mesta uz s najdenymi idealnymi penalmi a zistim, s akym mestom je najmensia penalizacia
     towns.forEach( (town, index) => { // v 604
       pen.push(Math.pow(ideal - town, 2)); // 400 - 602 = power -> 40 804
       for (let pIndex = 0; pIndex < index; pIndex++) {
