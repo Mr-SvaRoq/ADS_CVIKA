a55f47ae4795d470576f0801063575b40647671f
Dinh Nguyen Tien <bibo.dnt.159@gmail.com>
2021-02-26 10:17:39 +0100

    CV2

--- src/app/cviko2/cviko2.component.ts
+++ src/app/cviko2/cviko2.component.ts
@@ -32,17 +32,18 @@ export class Cviko2Component implements OnInit {
   }
 
   convertToKM(text: string): number[] {
-    const matrixString = text.split('\n').filter(el => el.length > 0 && !Number.isNaN(el)).map(el => parseInt(el, 10));
-    return matrixString;
+    const kmInteger = text.split('\n').filter(el => el.length > 0 && !Number.isNaN(el)).map(el => parseInt(el, 10));
+    return kmInteger;
   }
 
+  // pre kazde mesto hlada idealne "cestu" s najmensou penalizaciou
+  // a ak chcem zistit idealne penalizaciu dalsieho mesta, tak prejdem vsetky mesta uz s najdenymi idealnymi penalmi a zistim,
+  //      s akym mestom je najmensia penalizacia
   dynamicProg(towns: number[]): number {
     const ideal = 400;
     const pen = [];
 
-    // pre kazde mesto hlada idealne "cestu" s najmensou penalizaciou
-    // a ak chcem zistit idealne penalizaciu dalsieho mesta, tak prejdem vsetky mesta uz s najdenymi idealnymi penalmi a zistim,
-    //      s akym mestom je najmensia penalizacia
+
     towns.forEach( (town, index) => { // v 604
       pen.push(Math.pow(ideal - town, 2)); // 400 - 602 = power -> 40 804
       for (let pIndex = 0; pIndex < index; pIndex++) {
